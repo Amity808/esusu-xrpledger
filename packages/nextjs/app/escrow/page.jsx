8ba6e00a02ef9f8a445c7f5309168f4bb9852642
Amity808 <bolarinwamuhdsodiq0@gmail.com>
2024-08-10 07:51:31 +0100

    update escrow payment UI

--- packages/nextjs/app/escrow/page.jsx
+++ packages/nextjs/app/escrow/page.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import CreateEscrow from "~~/components/CreateEscrow"
+import bg from "~~/public/homebg.png"
 const Escrow = () => {
+  
+  const headerStyle = {
+    backgroundImage: `url(${bg.src})`,
+    /* Additional styles can be added here */
+
+    // Set background size to cover the container by default
+    backgroundSize: "cover",
+
+    // Center the background image by default
+    backgroundPosition: "center",
+
+    // Media query for mobile devices
+    "@media (maxWidth: 768px)": {
+      backgroundSize: "auto", // Adjust background size for smaller screens
+      backgroundPosition: "center", // You can adjust this as needed
+    },
+  };
   return (
-    <div>
-      
+    <div className={` h-screen`} style={headerStyle}>
+      <CreateEscrow />
     </div>
   )
 }
--- packages/nextjs/components/CreateEscrow.jsx
+++ packages/nextjs/components/CreateEscrow.jsx
@@ -3,6 +3,7 @@
 import React , { useState } from "react";
 import { Client, xrpToDrops } from "xrpl";
 import {useAuth} from "~~/context/AuthContext"
+import CustomInput from "./ui/CustomeInput";
 
 
 const CreateEscrow = () => {
@@ -21,33 +22,77 @@ const { createEscrow } = useAuth()
     return date;
   };
 
-  const addCreatescrow = async () => {
+  // const addCreatescrow = async () => {
+  //   let escrow_finish_date = new Date();
+  //   let escrow_cancel_date = new Date();
+  //   escrow_finish_date = addSeconds(parseInt(startEscrow));
+  //   escrow_cancel_date = addSeconds(parseInt(endEscrow));
+  //   console.log(escrow_finish_date);
+
+  //   const escrowTx = await client.autofill({
+  //     "TransactionType": "EscrowCreate",
+  //     "Account": "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh", //correct with good address
+  //     "Amount": xrpToDrops(amount),
+  //     "Destination": "",
+  //     "FinishAfter": escrow_finish_date,
+  //     "CancelAfter": escrow_cancel_date
+  //   })
+
+  //   // const signed = 
+  // };
+
+  const initalEscrow = async (e) => {
+    e.preventDefult();
     let escrow_finish_date = new Date();
     let escrow_cancel_date = new Date();
     escrow_finish_date = addSeconds(parseInt(startEscrow));
     escrow_cancel_date = addSeconds(parseInt(endEscrow));
     console.log(escrow_finish_date);
-
-    const escrowTx = await client.autofill({
-      "TransactionType": "EscrowCreate",
-      "Account": "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh", //correct with good address
-      "Amount": xrpToDrops(amount),
-      "Destination": "",
-      "FinishAfter": escrow_finish_date,
-      "CancelAfter": escrow_cancel_date
-    })
-
-    // const signed = 
-  };
-
-  const initalEscrow = async () => {
-
+    try {
+      const result = await createEscrow(escrow_finish_date, Destination)
+      console.log(result)
+    } catch (error) {
+      console.log(error)
+    }
   }
-  return <div>CreateEscrow
+  return <div className=" flex justify-center items-center">
 
-    <button onClick={addCreatescrow}>Create</button>
+    <div>
+      Create your escrow payment
+    </div>
+
+ 
     <form >
-  <button onClick={initalEscrow}>Create Escrow</button>
+      <div>
+      <CustomInput 
+            className=" w-[400px] text-black"
+              placeholder="Amount"
+              onChange={(e) => setEndEscrow(e.target.value)}
+              type="number"
+            
+            />
+      </div>
+      <div>
+      <CustomInput 
+            className=" w-[400px] text-black"
+              placeholder="Amount"
+              onChange={(e) => setDestination(e.target.value)}
+              type="number"
+            
+            />
+      </div>
+      <div>
+      <CustomInput 
+            className=" w-[400px] text-black"
+              placeholder="Amount"
+              onChange={(e) => setAmount(e.target.value)}
+              type="number"
+            
+            />
+      </div>
+
+  
+  <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg" onClick={initalEscrow}>Create Escrow</button>
     </form>
   </div>;
 };
