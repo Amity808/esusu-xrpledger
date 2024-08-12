2616103dcefc1964b63df4e6019434efca827b30
Amity808 <bolarinwamuhdsodiq0@gmail.com>
2024-08-12 13:30:22 +0100

    fix bug

--- packages/nextjs/app/login/page.jsx
+++ packages/nextjs/app/login/page.jsx
@@ -1,8 +1,8 @@
 import Web3Authentication from "../../components/Web3Authentication";
-import {useAuth} from "~~/context/AuthContext"
+
 export default function Home() {
   // Client
-  const { createEscrow, cancelEscrow, finishEscrow } = useAuth()
+  // const { createEscrow, cancelEscrow, finishEscrow } = useAuth()
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       
