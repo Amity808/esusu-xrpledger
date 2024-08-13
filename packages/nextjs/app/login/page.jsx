990c0908d784b54858a84f32fa73f5b7f01a0f9b
Amity808 <bolarinwamuhdsodiq0@gmail.com>
2024-08-13 06:52:55 +0100

    implement get account escrow details with xrpl

--- packages/nextjs/app/login/page.jsx
+++ packages/nextjs/app/login/page.jsx
@@ -1,8 +1,6 @@
 import Web3Authentication from "../../components/Web3Authentication";
 
 export default function Home() {
-  // Client
-  // const { createEscrow, cancelEscrow, finishEscrow } = useAuth()
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       
--- packages/nextjs/components/EscrowHistory.jsx
+++ packages/nextjs/components/EscrowHistory.jsx
@@ -1,33 +1,42 @@
 import React, { useState } from "react";
-import {useAuth} from "~~/context/AuthContext"
+import { useAuth } from "~~/context/AuthContext";
+
 export default function EscrowHistory() {
   // Client
-  const { createEscrow, cancelEscrow, finishEscrow } = useAuth()
-  const [sequence, setSequence] = useState('')
+  const { createEscrow, cancelEscrow, finishEscrow } = useAuth();
+  const [sequence, setSequence] = useState("");
+  const [fininshSequence, setFininshSequence] = useState("");
 
-  const finishEscrowStatus = async() => {
+  const finishEscrowStatus = async () => {
     try {
-        const result = await finishEscrow(sequence)
-        console.log(result)
+      const result = await finishEscrow(fininshSequence);
+      console.log(result);
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
-  }
-  const cancelEscrowS = async() => {
+  };
+  const cancelEscrowS = async () => {
     try {
-        const result = await cancelEscrow(sequence)
-        console.log(result)
+      const result = await cancelEscrow(sequence);
+      console.log(result);
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
-  }
+  };
   return (
     <div>
-        <h2>Escrow History</h2>
+      <h2>Escrow History</h2>
 
+      <div>
         <div>
-
+          <h2>Input your escrow sequence to check your escrow status</h2>
+          <input type="text" placeholder="Finish Escrow" onChange={() => setFininshSequence(e.target.value)} className="input input-bordered w-full max-w-xs" />
         </div>
+        <div>
+          <h2>Input your escrow sequence to cancel your escrow</h2>
+          <input type="text" placeholder="Cancel Escrow" onChange={() => setSequence(e.target.value)} className="input input-bordered w-full max-w-xs" />
+        </div>
+      </div>
     </div>
   );
 }
